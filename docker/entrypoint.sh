bd8cb3016b598bf3bddfabfdfaed8e94917d5ae4
julianwki <julian@wiersbitzki.com>
2019-11-14 15:10:14 +0100

    use /tmp

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /usr/src/app
+cd $TMP
 wget https://github.com/MichaelMure/WalletGenerator.net/archive/master.zip
 unzip master.zip
 mv WalletGenerator.net-master/* /usr/share/nginx/html
